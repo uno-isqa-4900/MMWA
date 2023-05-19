99f105354357552d2487f432beb5325fabb926c8
yqiuc <98722550+yqiuc@users.noreply.github.com>
2023-05-18 23:55:48 -0500

    update

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,35 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar class="constrain">
+        <q-btn flat round dense class="q-mr-sm" to="/" icon="eva-play-circle-outline" size="18px"/>
+        <q-separator class="large-screen-only" vertical spaced />
+        <q-toolbar-title align="center">
+          My favorite movies
+        </q-toolbar-title>
+        <q-separator class="large-screen-only" vertical spaced />
+        <q-btn flat round dense to="/movie-list" icon="eva-home-outline" size="18px"/>
+        <q-btn flat round dense to="/movie-create" icon="eva-plus-outline" size="18px"/>
+      </q-toolbar>
+    </q-header>
+    <q-footer class="bg-white" bordered>
+      <q-tabs class="text-grey-10 small-screen-only" active-color="primary" indicator-color="transparent" >
+        <q-route-tab to="/movie-list" name="home" icon="eva-home-outline" />
+        <q-route-tab to="/movie-create" name="movie" icon="eva-plus-outline" />
+      </q-tabs>
+    </q-footer>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'MainLayout',
+  data () {
+    return {
+    }
+  },
+}
+</script>
---
+++ src/pages/PageWelcome.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="constrain-more q-pa-md">
+    <template>
+      <q-card class="card-post q-mb-md" flat bordered>
+        <q-card-section class="text-black">
+          <div class="text-h6">Welcome!</div>
+        </q-card-section>
+        <q-separator />
+        <img src="~assets/movies.jpg" height="350" width="30">
+        <q-card-actions align="right">
+          <q-btn flat>Movies</q-btn>
+        </q-card-actions>
+        <q-separator />
+      </q-card>
+    </template>
+  </q-page>
+</template>
